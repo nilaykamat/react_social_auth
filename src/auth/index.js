4c33ad82f426d034ce61612cf1c217c7143062d9
nilay <nilay@browntape.com>
2017-10-12 11:51:11 +0530

    Implemented Facebook login, Profile redirect pending

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
+import Facebook from './Facebook/'
 
 class Auth extends Component{
     
     render(){
         return(
             <div>
-				<h3 class="centered">Social Login</h3>
+				<h3 className="centered">Social Login</h3>
 				<hr/>
 				<div id="social-buttons">
-					<button>Facebook Login</button>
-					<button>Google Login</button>
-					<button>Twitter Login</button>
-					<button>Linkedin Login</button>
+					<Facebook />
+					<button id="btnGoogle">Google Login</button>
+					<button id="btnTwitter">Twitter Login</button>
+					<button id="btnLinkedin">Linkedin Login</button>
 				</div>
             </div>
         )
