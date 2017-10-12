476a7f0eab3844325c94aaf0c75bfca426944f3e
nilay <nilay@browntape.com>
2017-10-12 12:15:31 +0530

    Profile page modifications

--- src/profile/index.js
+++ src/profile/index.js
@@ -4,10 +4,26 @@ import React, {Component} from 'react';
 //import config from '../../../config';
 
 class Profile extends Component{
+	constructor(props) {
+		super(props);
+		var accessToken = localStorage.getItem('accessToken');
+		console.log(accessToken);
+		if(!accessToken){
+			// this.context.history.push('/')
+		}
+	}
     render(){
+		if(localStorage.getItem('image')){
+			var image = localStorage.getItem('image');
+		} else{
+			var image = "https://svet.fri.uni-lj.si/wp-content/uploads/2014/12/Avatar-150x150.png";
+		}
         return(
-            <div>
-                <h2>This is your profile</h2>   
+            <div className="centered">
+				<img id="imgProfilePic" src={image} />
+				<hr />
+				<h2>Hello {localStorage.getItem('name')}</h2>
+				<h3>from {localStorage.getItem('network')}</h3>
                 
             </div>
         )
